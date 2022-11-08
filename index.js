3b15d0fbc903d689b48c9ab1106bcccdbc90b98d
Chandan Bauri <chandanbauri441@gmail.com>
2022-11-09 00:04:14 +0530

    boiler-plate

---
+++ index.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const express = require('express');
+const { Server } = require('http')
+const { version } = require('./package.json');
+const { sequelize } = require('./database/models/index');
+
+const app = express();
+const PORT = process.env.PORT || 4000
+const server = new Server(app);
+
+
+
+
+// Middlewares 
+/* 
+
+
+app.use(<middlewares>);
+
+
+*/
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Basic Routes
+app.get('/version', (req, res) => {
+    res.send({
+        "APP-VERSION": version
+    })
+})
+
+app.use('*', (req, res) => {
+    res.send(`
+    <div>
+    The path does not exist
+    </div>
+    `)
+})
+
+
+// Routes
+/* 
+app.use('/<path>',route-module)
+ */
+
+
+
+
+
+server.listen(PORT, async () => {
+    console.log('SEVER started ! \non port %d !', PORT)
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})
