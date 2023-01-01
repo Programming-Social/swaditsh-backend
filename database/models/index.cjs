a8ab94866785b68266ecea24f20ee3db193617cf
chandan bauri <chandanbauri441@gmail.com>
2023-01-02 03:36:19 +0530

    fixing deployment environment bug v18

--- database/models/index.cjs
+++ database/models/index.cjs
@@ -5,7 +5,8 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require("../config/config.cjs")[env];
+const ConfigImport = require("../config/config.cjs");
+const config = ConfigImport[env]
 const db = {};
 
 let sequelize;
