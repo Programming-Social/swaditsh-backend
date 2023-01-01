e3e83663bb23d5a8edd53106f178acdd9dd6010c
chandan bauri <chandanbauri441@gmail.com>
2023-01-02 02:05:08 +0530

    fixing deployment environment bug v4

--- database/models/index.cjs
+++ database/models/index.cjs
@@ -14,7 +14,7 @@ sequelize = new Sequelize(config.url);
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".cjs"
     );
   })
   .forEach((file) => {
